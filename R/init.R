3864a560651600b245b2b0d62b4b895bfcec0ed6
Josef Hardi <josef.hardi@gmail.com>
2021-01-08 10:59:22 -0800

    Initial commit

---
+++ R/init.R
@@ -0,0 +1,99 @@
+.onLoad <- function(libname, pkgname) {
+  
+  # REST APIs
+  DCAPI <- "http://api.datacommons.org/"
+  DCAPI_STAT_ALL <<- paste0(DCAPI, "stat/all")
+  
+  # Return payload keywords
+  PLACE_DATA <<- "placeData"
+  STATVAR_DATA <<- "statVarData"
+  SOURCE_SERIES <<- "sourceSeries"
+  VAL <<- "val"
+  MEASUREMENT_METHOD <<- "measurementMethod"
+  IMPORT_NAME <<- "importName"
+  PROVENANCE_DOMAIN <<- "provenanceDomain"
+  PROVENANCE_URL <<- "provenanceUrl"
+  
+  # Statistical variables group
+  CENSUS_5_YEARS_BRACKETS <<- c(
+    "Under5Years",
+    "5To9Years",
+    "10To14Years",
+    "15To19Years",
+    "20To24Years",
+    "25To29Years",
+    "30To34Years",
+    "35To39Years",
+    "40To44Years",
+    "45To49Years",
+    "50To54Years",
+    "55To59Years",
+    "60To64Years",
+    "65To69Years",
+    "70To74Years",
+    "75To79Years",
+    "80To84Years",
+    "85To89Years",
+    "90To94Years",
+    "94To99Years",
+    "Above100Years"
+  )
+  
+  # ZIP codes
+  # City level
+  ALVISO_CITY_CA <<- c("95002")
+  CAMPBELL_CITY_CA <<- c("95008", "95009", "95011")
+  COYOTE_CITY_CA <<- c("95013")
+  CUPERTINO_CITY_CA <<- c("95014", "95015")
+  GILROY_CITY_CA <<- c("95020", "95021")
+  HOLY_CITY_CITY_CA <<- c("95026")
+  LOS_ALTOS_CITY_CA <<- c("94022", "94023", "94024")
+  LOS_GATOS_CITY_CA <<- c("95030", "95031", "95032")
+  MILPITAS_CITY_CA <<- c("95035", "95036")
+  MORGAN_HILL_CITY_CA <<- c("95037", "95038")
+  MOUNT_HAMILTON_CITY_CA <<- c("95140")
+  MOUNTAIN_VIEW_CITY_CA <<- c("94035", "94039", "94040", "94041", "94042",
+                              "94043")
+  NEW_ALMADEN_CITY_CA <<- c("95042")
+  PALO_ALTO_CITY_CA <<- c("94301", "94302", "94303", "94304", "94306", "94309")
+  REDWOOD_ESTATES_CITY_CA <<- c("95044")
+  SAN_JOSE_CITY_CA <<- c("95101", "95103", "95106", "95108", "95109", "95110",
+                         "95111", "95112", "95113", "95115", "95116", "95117",
+                         "95118", "95119", "95120", "95121", "95122", "95123",
+                         "95124", "95125", "95126", "95127", "95128", "95129",
+                         "95130", "95131", "95132", "95133", "95134", "95135",
+                         "95136", "95138", "95139", "95141", "95148", "95150",
+                         "95151", "95152", "95153", "95154", "95155", "95156",
+                         "95157", "95158", "95159", "95160", "95161", "95164",
+                         "95170", "95172", "95173", "95190", "95191", "95192",
+                         "95193", "95194", "95196")
+  SAN_MARTIN_CITY_CA <<- c("95046")
+  SANTA_CLARA_CITY_CA <<- c("95050", "95051", "95052", "95053", "95054", 
+                            "95055", "95056")
+  SARATOGA_CITY_CA <<- c("95070", "95071")
+  STANFORD_CITY_CA <<- c("94305")
+  SUNNYVALE_CITY_CA <<- c("94085", "94086", "94087", "94088", "94089")
+  
+  # County level
+  SANTA_CLARA_COUNTY_CA <<- c(ALVISO_CITY_CA, 
+                              CAMPBELL_CITY_CA, 
+                              COYOTE_CITY_CA,
+                              CUPERTINO_CITY_CA,
+                              GILROY_CITY_CA,
+                              HOLY_CITY_CITY_CA,
+                              LOS_ALTOS_CITY_CA,
+                              LOS_GATOS_CITY_CA,
+                              MILPITAS_CITY_CA,
+                              MORGAN_HILL_CITY_CA,
+                              MOUNT_HAMILTON_CITY_CA,
+                              MOUNTAIN_VIEW_CITY_CA,
+                              NEW_ALMADEN_CITY_CA,
+                              PALO_ALTO_CITY_CA,
+                              REDWOOD_ESTATES_CITY_CA,
+                              SAN_JOSE_CITY_CA,
+                              SAN_MARTIN_CITY_CA,
+                              SANTA_CLARA_CITY_CA,
+                              SARATOGA_CITY_CA,
+                              STANFORD_CITY_CA,
+                              SUNNYVALE_CITY_CA)
+}
---
+++ R/statvars.R
@@ -0,0 +1,214 @@
+#' Return the male population by 5-year age brackets, given the ZIP codes and
+#' the observation year period.
+#' 
+#' @param zips required, vector of string(s) of US ZIP codes
+#' @param start_year optional, integer indicating the start year of observation.
+#'    2011 by default.
+#' @param end_year optional, integer indicating the end year of observation.
+#'    2018 by default.
+#' @param year optional, integer indicating a point of year of observation. If
+#'    this parameter has a value then start_year=year and end_year=year.
+#'    NA by default.
+#' @return A named list with each list item is a data frame of the male
+#'    population observations in a specific year. Each row in the data frame
+#'    details the 5-year age brackets of a particular zip code.
+#'     
+#' @export
+#' @examples
+#' zips <- c("94035","94039","94040","94041","94042","94043")
+#' 
+#' # Count the male population in a specific area
+#' count_male_population(zips)
+#' 
+#' # Count the male population from 2012 to 2015
+#' count_male_population(zips, start_year=2012, end_year=2015)
+#' 
+#' # Count the male population in 2012
+#' count_male_population(zips, year=2012)
+count_male_population <- function(zips, start_year=2011, end_year=2018, year=NA) {
+
+  zip_map <- .create_zip_dcid_map(zips)
+  
+  statvar_map <- sapply(CENSUS_5_YEARS_BRACKETS, 
+                        function(x) paste0("Count_Person_", x, "_Male"), 
+                        simplify = FALSE, USE.NAMES = TRUE)
+  
+  return (.count_population_by_gender(zip_map, statvar_map, start_year, end_year, year))
+}
+
+#' Return the female population by 5-year age brackets, given the ZIP codes and
+#' the observation year period.
+#' 
+#' @param zips required, vector of string(s) of US ZIP codes
+#' @param start_year optional, integer indicating the start year of observation.
+#'    2011 by default.
+#' @param end_year optional, integer indicating the end year of observation.
+#'    2018 by default.
+#' @param year optional, integer indicating a point of year of observation. If
+#'    this parameter has a value then start_year=year and end_year=year.
+#'    NA by default.
+#' @return A named list with each list item is a data frame of the male
+#'    population observations in a specific year. Each row in the data frame
+#'    details the 5-year age brackets of a particular zip code.
+#'     
+#' @export
+#' @examples
+#' zips <- c("94035","94039","94040","94041","94042","94043")
+#' 
+#' # Count the male population in a specific area
+#' count_male_population(zips)
+#' 
+#' # Count the male population from 2012 to 2015
+#' count_male_population(zips, start_year=2012, end_year=2015)
+#' 
+#' # Count the male population in 2012
+#' count_male_population(zips, year=2012)
+count_female_population <- function(zips, start_year=2011, end_year=2018, year=NA) {
+  
+  zip_map <- .create_zip_dcid_map(zips)
+  
+  statvar_map <- sapply(CENSUS_5_YEARS_BRACKETS, 
+                        function(x) paste0("Count_Person_", x, "_Female"), 
+                        simplify = FALSE, USE.NAMES = TRUE)
+  
+  return (.count_person_by_gender(zip_map, statvar_map, start_year, end_year, year))
+}
+
+# Helpers ----------------------------------------------------------------------------------
+
+.count_population_by_gender <- function(zip_map, statvar_map, start_year, end_year, year) {
+  
+  start_year <- if (!is.na(year)) year else start_year
+  end_year <- if (!is.na(year)) year else end_year
+  
+  body <- jsonlite::toJSON(list(
+    stat_vars = as.vector(unlist(statvar_map)), 
+    places = as.vector(unlist(zip_map))), 
+    auto_unbox = TRUE)
+  
+  http_response <- .http_post(DCAPI_STAT_ALL, body);
+  
+  output <- list()
+  for (year in as.character(start_year:end_year)) {
+    # initialize main data frame
+    df1 <- data.frame(zip=names(zip_map))
+    for (year_bracket in names(statvar_map)) {
+      # initialize data frame to store each statistical variable per zip code
+      df2 <- data.frame(zip=names(zip_map))
+      statvar_values <- c()
+      for (zip in names(zip_map)) {
+        zip_dcid <- zip_map[[zip]]
+        place_data <- .get_place_data(http_response, zip_dcid)
+        
+        statvar_dcid <- statvar_map[[year_bracket]]
+        statvar_data <- .get_statvar_data(place_data, statvar_dcid)
+        
+        value <- .get_statvar_value_from_year(statvar_data, year)
+        statvar_values <- c(statvar_values, value)
+      }
+      df2[, year_bracket] <- factor(statvar_values)
+      df1 <- merge(x=df1, y=df2, by="zip", all.x=TRUE)
+    }
+    provenance_df <- .get_provenance_info(http_response, zip_map, statvar_map)
+    
+    df1 <- merge(x=df1, y=provenance_df, by="zip", all.x=TRUE)
+    rownames(df1) <- df1$zip
+    output[[year]] <- df1
+  }
+  return (output)
+}
+
+.get_provenance_info <- function(obj, zip_map, statvar_map) {
+  
+  output <- data.frame(zip=names(zip_map), measurementMethod=NA,
+                       provenanceDomain=NA, provenanceUrl=NA)
+  
+  measurement_methods <- c()
+  provenance_domains <- c()
+  provenance_urls <- c()
+  for (zip in names(zip_map)) {
+    zip_dcid <- zip_map[[zip]]
+    place_data <- .get_place_data(obj, zip_dcid)
+    measurement_method <- NA
+    provenance_domain <- NA
+    provenance_url <- NA
+    for (year_bracket in names(statvar_map)) {
+      statvar_dcid <- statvar_map[[year_bracket]]
+      statvar_data <- .get_statvar_data(place_data, statvar_dcid)
+      
+      measurement_method <- .coalesce(measurement_method, 
+                                      .get_statvar_measurement_method(statvar_data))
+      provenance_domain <- .coalesce(provenance_domain, 
+                                     .get_statvar_provenance_domain(statvar_data))
+      provenance_url <- .coalesce(provenance_url, 
+                                  .get_statvar_provenance_url(statvar_data))
+    }
+    measurement_methods <- c(measurement_methods, measurement_method)
+    provenance_domains <- c(provenance_domains, provenance_domain)
+    provenance_urls <- c(provenance_urls, provenance_url)
+  }
+  output$measurementMethod <- factor(measurement_methods)
+  output$provenanceDomain <- factor(provenance_domains)
+  output$provenanceUrl <- factor(provenance_urls)
+  
+  return (output)
+}
+
+.http_post <- function(api, body) {
+  response <- httr::POST(api, body = body)
+  if (httr::http_type(response) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  return (.parse(response))
+}
+
+.parse <- function(response) {
+  return (httr::content(response, "parsed"))
+}
+
+.create_zip_dcid_map <- function(zips) {
+  zip_map <- sapply(zips, 
+                    function(x) paste0("zip/", x), 
+                    simplify = FALSE, USE.NAMES = TRUE)
+  return (zip_map)
+}
+
+.get_place_data <- function(obj, zip_dcid) {
+  return (obj[[PLACE_DATA]][[zip_dcid]])
+}
+
+.get_statvar_data <- function(obj, statvar_dcid) {
+  return (obj[[STATVAR_DATA]][[statvar_dcid]])
+}
+
+.get_source_series <- function(obj) {
+  return (obj[1][[SOURCE_SERIES]][1][[1]])
+}
+
+.get_statvar_value_from_year <- function(obj, year) {
+  source_series_values <- .get_source_series(obj)[[VAL]]
+  value <- source_series_values[[year]]
+  return (.check_not_empty(value))
+}
+
+.get_statvar_measurement_method <- function(obj) {
+  return (.get_source_series(obj)[[MEASUREMENT_METHOD]])
+}
+
+.get_statvar_provenance_domain <- function(obj) {
+  return (.get_source_series(obj)[[PROVENANCE_DOMAIN]])
+}
+
+.get_statvar_provenance_url <- function(obj) {
+  return (.get_source_series(obj)[[PROVENANCE_URL]])
+}
+
+.check_not_empty <- function(value) {
+  return (if (is.null(value)) NA else value)
+}
+
+.coalesce <- function(v1, v2) {
+  v1 <- .check_not_empty(v1)
+  v2 <- .check_not_empty(v2)
+  return (dplyr::coalesce(v1, v2))
+}
