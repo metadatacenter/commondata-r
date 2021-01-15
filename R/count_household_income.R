fad1edc2421a21baaf275f06bc72b045ec3b1efe
Josef Hardi <josef.hardi@gmail.com>
2021-01-15 14:18:31 -0800

    Added doc for each function

--- R/count_household_income.R
+++ R/count_household_income.R
@@ -1,3 +1,35 @@
+#' Return the householders count per income bracket for the given ZIP codes and
+#' observation year(s) period.
+#' 
+#' @param geo_names required, vector of string(s) of geographical names
+#' @param start_year optional, integer indicating the start year of observation.
+#'    2011 by default.
+#' @param end_year optional, integer indicating the end year of observation.
+#'    2018 by default.
+#' @param year optional, integer indicating a single year of observation.
+#'    This parameter overrides the start_year and end_year parameters when
+#'    it is not NA, such that start_year=year and end_year=year. NA by default.
+#' @return  A named list with each list item is a data frame containing the 
+#'    household income observations of each region per income bracket. The data
+#'    frame is identified by the observation year.
+#'     
+#' @export
+#' @examples
+#' zips <- c("94035","94039","94040","94041","94042","94043")
+#' 
+#' # Count the householders per income bracket in the specified ZIP codes
+#' count_household_income(zips)
+#' 
+#' # Count the householders from 2012 to 2015
+#' count_household_income(zips, start_year=2012, end_year=2015)
+#' 
+#' # Count the householders in 2012
+#' count_household_income(zips, year=2012)
+#' 
+#' # Count the householders in the state of California
+#' count_household_income(c("California")) # State name
+#' count_household_income(c("CA")) # State code
+#' count_household_income(c("06")) # 2-digit state FIPS code
 count_household_income <- function(geo_names, start_year=2011, end_year=2018, year=NA) {
   
   geo_map <- .create_geo_dcid_map(geo_names)
--- R/count_household_income_by_denominator.R
+++ R/count_household_income_by_denominator.R
@@ -1,3 +1,35 @@
+#' Return the householders count per income bracket and per age group for the
+#' given ZIP codes and observation year(s) period.
+#' 
+#' @param geo_names required, vector of string(s) of geographical names
+#' @param start_year optional, integer indicating the start year of observation.
+#'    2011 by default.
+#' @param end_year optional, integer indicating the end year of observation.
+#'    2018 by default.
+#' @param year optional, integer indicating a single year of observation.
+#'    This parameter overrides the start_year and end_year parameters when
+#'    it is not NA, such that start_year=year and end_year=year. NA by default.
+#' @return  A named list with each list item is a data frame containing the 
+#'    household income observations of each region per income bracket. The data
+#'    frame is identified by the observation year and age group.
+#'
+#' @export
+#' @examples
+#' zips <- c("94035","94039","94040","94041","94042","94043")
+#' 
+#' # Count the householders per income and age group in the specified ZIP codes
+#' count_household_income_by_age(zips)
+#' 
+#' # Count the householders from 2012 to 2015
+#' count_household_income_by_age(zips, start_year=2012, end_year=2015)
+#' 
+#' # Count the householders in 2012
+#' count_household_income_by_age(zips, year=2012)
+#' 
+#' # Count the householders in the state of California
+#' count_household_income_by_age(c("California")) # State name
+#' count_household_income_by_age(c("CA")) # State code
+#' count_household_income_by_age(c("06")) # 2-digit state FIPS code
 count_household_income_by_age <- function(geo_names, start_year=2011, end_year=2018, year=NA) {
   
   geo_map <- .create_geo_dcid_map(geo_names)
@@ -14,6 +46,38 @@ count_household_income_by_age <- function(geo_names, start_year=2011, end_year=2
                                                  start_year, end_year, year))
 }
 
+#' Return the householders count per income bracket and per race group for the
+#' given ZIP codes and observation year(s) period.
+#' 
+#' @param geo_names required, vector of string(s) of geographical names
+#' @param start_year optional, integer indicating the start year of observation.
+#'    2011 by default.
+#' @param end_year optional, integer indicating the end year of observation.
+#'    2018 by default.
+#' @param year optional, integer indicating a single year of observation.
+#'    This parameter overrides the start_year and end_year parameters when
+#'    it is not NA, such that start_year=year and end_year=year. NA by default.
+#' @return  A named list with each list item is a data frame containing the 
+#'    household income observations of each region per income bracket. The data
+#'    frame is identified by the observation year and rage group.
+#'
+#' @export
+#' @examples
+#' zips <- c("94035","94039","94040","94041","94042","94043")
+#' 
+#' # Count the householders per income and race group in the specified ZIP codes
+#' count_household_income_by_race(zips)
+#' 
+#' # Count the householders from 2012 to 2015
+#' count_household_income_by_race(zips, start_year=2012, end_year=2015)
+#' 
+#' # Count the householders in 2012
+#' count_household_income_by_race(zips, year=2012)
+#' 
+#' # Count the householders in the state of California
+#' count_household_income_by_race(c("California")) # State name
+#' count_household_income_by_race(c("CA")) # State code
+#' count_household_income_by_race(c("06")) # 2-digit state FIPS code
 count_household_income_by_race <- function(geo_names, start_year=2011, end_year=2018, year=NA) {
   
   geo_map <- .create_geo_dcid_map(geo_names)
